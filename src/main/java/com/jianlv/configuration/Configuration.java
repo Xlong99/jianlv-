47c29753d6d2e196f5ca4d66701c1d31bc35e4ed
duyiming <nahso4@outlook.com>
2019-05-19 21:57:17 +0800

    创建项目

---
+++ src/main/java/com/jianlv/configuration/Configuration.java
@@ -0,0 +1,15 @@
+package com.jianlv.configuration;
+
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * spring配置类
+ *
+ * @author 杜奕明
+ * @date 2019/5/19 21:55
+ */
+@org.springframework.context.annotation.Configuration
+@EnableTransactionManagement
+public class Configuration {
+
+}
