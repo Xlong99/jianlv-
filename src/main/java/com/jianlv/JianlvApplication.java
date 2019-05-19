a0f2e03d28edbcb9d41aa372871bd2accecb0995
duyiming <nahso4@outlook.com>
2019-05-19 22:00:18 +0800

    创建项目

---
+++ src/main/java/com/jianlv/JianlvApplication.java
@@ -0,0 +1,19 @@
+package com.jianlv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring boot启动类
+ *
+ * @author 杜奕明
+ * @date 2019/5/19 21:52
+ */
+@SpringBootApplication
+public class JianlvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JianlvApplication.class, args);
+    }
+
+}
